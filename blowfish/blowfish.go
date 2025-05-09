f8210f89effdf505f4f8c970dda9fb9690044dfc
Sabina Zholobova <s.zholobova@yadro.com>
2025-05-09 12:14:03 +0300

    done: low level

--- blowfish/blowfish.go
+++ blowfish/blowfish.go
@@ -6,10 +6,53 @@ package blowfish
 // #cgo CFLAGS: -Wno-deprecated-declarations
 // #include <openssl/blowfish.h>
 import "C"
+import "unsafe"
 
 type Blowfish struct {
+	key []byte
+}
+
+func (b Blowfish) BlockSize() int {
+	return 8
+}
+
+func (b Blowfish) Encrypt(bytes []byte, bytes2 []byte) {
+	var bfKey C.BF_KEY
+	var keyLen = len(b.key)
+
+	C.BF_set_key(
+		&bfKey,
+		C.int(keyLen),
+		(*C.uchar)(unsafe.Pointer(&b.key[0])),
+	)
+
+	C.BF_ecb_encrypt(
+		(*C.uchar)(unsafe.Pointer(&bytes2[0])),
+		(*C.uchar)(unsafe.Pointer(&bytes[0])),
+		&bfKey,
+		C.BF_ENCRYPT,
+	)
+}
+
+func (b Blowfish) Decrypt(bytes []byte, bytes2 []byte) {
+	var bfKey C.BF_KEY
+	var keyLen = len(b.key)
+	C.BF_set_key(
+		&bfKey,
+		C.int(keyLen),
+		(*C.uchar)(unsafe.Pointer(&b.key[0])),
+	)
+
+	C.BF_ecb_encrypt(
+		(*C.uchar)(unsafe.Pointer(&bytes2[0])),
+		(*C.uchar)(unsafe.Pointer(&bytes[0])),
+		&bfKey,
+		C.BF_DECRYPT,
+	)
 }
 
 func New(key []byte) *Blowfish {
-	panic("implement me")
+	return &Blowfish{
+		key: key,
+	}
 }
--- gzep/encode.go
+++ gzep/encode.go
@@ -3,15 +3,214 @@
 package gzep
 
 import (
+	"compress/flate"
 	"compress/gzip"
+	"encoding/binary"
+	"errors"
+	"hash/crc32"
 	"io"
+	"sync"
+	"time"
 )
 
+const (
+	gzipID1     = 0x1f
+	gzipID2     = 0x8b
+	gzipDeflate = 8
+)
+
+const (
+	BestSpeed       = flate.BestSpeed
+	BestCompression = flate.BestCompression
+)
+
+var le = binary.LittleEndian
+
+type MyGzipWriter struct {
+	gzip.Header // written at first call to Write, Flush, or Close
+	w           io.Writer
+	level       int
+	wroteHeader bool
+	closed      bool
+	buf         [10]byte
+	compressor  *flate.Writer
+	digest      uint32 // CRC-32, IEEE polynomial (section 8)
+	size        uint32 // Uncompressed size (section 2.3.1)
+	err         error
+}
+
+func (z *MyGzipWriter) init(w io.Writer, level int) {
+	compressor := z.compressor
+	if compressor != nil {
+		compressor.Reset(w)
+	}
+	*z = MyGzipWriter{
+		Header: gzip.Header{
+			OS: 255, // unknown
+		},
+		w:          w,
+		level:      level,
+		compressor: compressor,
+	}
+}
+
+func (z *MyGzipWriter) writeString(s string) (err error) {
+	// GZIP stores Latin-1 strings; error if non-Latin-1; convert if non-ASCII.
+	needconv := false
+	for _, v := range s {
+		if v == 0 || v > 0xff {
+			return errors.New("gzip.Write: non-Latin-1 header string")
+		}
+		if v > 0x7f {
+			needconv = true
+		}
+	}
+	if needconv {
+		b := make([]byte, 0, len(s))
+		for _, v := range s {
+			b = append(b, byte(v))
+		}
+		_, err = z.w.Write(b)
+	} else {
+		_, err = io.WriteString(z.w, s)
+	}
+	if err != nil {
+		return err
+	}
+	// GZIP strings are NUL-terminated.
+	z.buf[0] = 0
+	_, err = z.w.Write(z.buf[:1])
+	return err
+}
+
+func (z *MyGzipWriter) Flush() error {
+	if z.err != nil {
+		return z.err
+	}
+	if z.closed {
+		return nil
+	}
+	if !z.wroteHeader {
+		z.Write(nil)
+		if z.err != nil {
+			return z.err
+		}
+	}
+	z.err = z.compressor.Flush()
+	return z.err
+}
+
+func (z *MyGzipWriter) Write(p []byte) (int, error) {
+	if z.err != nil {
+		return 0, z.err
+	}
+	var n int
+	// Write the GZIP header lazily.
+	if !z.wroteHeader {
+		z.wroteHeader = true
+		z.buf = [10]byte{0: gzipID1, 1: gzipID2, 2: gzipDeflate}
+		if z.Extra != nil {
+			z.buf[3] |= 0x04
+		}
+		if z.Name != "" {
+			z.buf[3] |= 0x08
+		}
+		if z.Comment != "" {
+			z.buf[3] |= 0x10
+		}
+		if z.ModTime.After(time.Unix(0, 0)) {
+			le.PutUint32(z.buf[4:8], uint32(z.ModTime.Unix()))
+		}
+		if z.level == BestCompression {
+			z.buf[8] = 2
+		} else if z.level == BestSpeed {
+			z.buf[8] = 4
+		}
+		z.buf[9] = z.OS
+		_, z.err = z.w.Write(z.buf[:10])
+		if z.err != nil {
+			return 0, z.err
+		}
+		if z.Extra != nil {
+			z.err = z.writeBytes(z.Extra)
+			if z.err != nil {
+				return 0, z.err
+			}
+		}
+		if z.Name != "" {
+			z.err = z.writeString(z.Name)
+			if z.err != nil {
+				return 0, z.err
+			}
+		}
+		if z.Comment != "" {
+			z.err = z.writeString(z.Comment)
+			if z.err != nil {
+				return 0, z.err
+			}
+		}
+		if z.compressor == nil {
+			z.compressor, _ = flate.NewWriter(z.w, z.level)
+		}
+	}
+	z.size += uint32(len(p))
+	z.digest = crc32.Update(z.digest, crc32.IEEETable, p)
+	n, z.err = z.compressor.Write(p)
+	return n, z.err
+}
+
+func (z *MyGzipWriter) writeBytes(b []byte) error {
+	if len(b) > 0xffff {
+		return errors.New("gzip.Write: Extra data is too large")
+	}
+	le.PutUint16(z.buf[:2], uint16(len(b)))
+	_, err := z.w.Write(z.buf[:2])
+	if err != nil {
+		return err
+	}
+	_, err = z.w.Write(b)
+	return err
+}
+
+func (z *MyGzipWriter) Close() error {
+	if z.err != nil {
+		return z.err
+	}
+	if z.closed {
+		return nil
+	}
+	z.closed = true
+	if !z.wroteHeader {
+		z.Write(nil)
+		if z.err != nil {
+			return z.err
+		}
+	}
+	z.err = z.compressor.Close()
+	if z.err != nil {
+		return z.err
+	}
+	le.PutUint32(z.buf[:4], z.digest)
+	le.PutUint32(z.buf[4:8], z.size)
+	_, z.err = z.w.Write(z.buf[:8])
+	return z.err
+}
+
+var bufferPool = sync.Pool{
+	New: func() interface{} {
+		return new(MyGzipWriter)
+	},
+}
+
 func Encode(data []byte, w io.Writer) error {
-	ww := gzip.NewWriter(w)
-	defer func() { _ = ww.Close() }()
+	ww := bufferPool.Get().(*MyGzipWriter)
+	ww.init(w, gzip.DefaultCompression)
+	defer bufferPool.Put(ww)
 	if _, err := ww.Write(data); err != nil {
 		return err
 	}
-	return ww.Flush()
+	if err := ww.Close(); err != nil {
+		return err
+	}
+	return nil
 }
